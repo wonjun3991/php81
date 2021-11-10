e02fe798a1a372efaad0e03b51889b484d74ff26
wonjun3991 <48777603+wonjun3991@users.noreply.github.com>
2021-11-10 23:37:38 +0900

    Fiber test

--- test/NewDtoTest.php
+++ test/NewDtoTest.php
@@ -14,23 +14,40 @@ class NewDtoTest extends TestCase
      * @test
      * @return void
      */
-    public function fiber(){
-        $fiber = new Fiber(fn($message) => print $message);
-        try {
-            $fiber->start('Hi');
-        } catch (Throwable $e) {
+    public function fiber()
+    {
+        $messages = [
+            'hello',
+            "I'm",
+            'Hori',
+        ];
 
+        $fiber = new Fiber(function ($messages) {
+            foreach ($messages as $message) {
+                Fiber::suspend($message);
+            }
+        });
+
+        $message = $fiber->start($messages);
+
+        $fiberRepeatCount = 1;
+
+        while (!$fiber->isTerminated()) {
+            echo "fiberCount : {$fiberRepeatCount} message : {$message}";
+            $message = $fiber->resume();
+            $fiberRepeatCount++;
         }
     }
 
     /**
      * @test
      */
-    public function dtoTest(){
+    public function dtoTest()
+    {
         $expectedName = 'test';
         $expectedAge = 123;
 
-        $newDto = new NewDto($expectedName,$expectedAge);
+        $newDto = new NewDto($expectedName, $expectedAge);
 
         $this->assertEquals($expectedName, $newDto->name);
         $this->assertEquals($expectedAge, $newDto->age);
